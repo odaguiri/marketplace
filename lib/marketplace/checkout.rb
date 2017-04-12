34858d5d68a1a6321156b5dd36bc96b5b085b077
Raul Odaguiri <raulodaguiri@gmail.com>
2017-04-12 10:09:36 -0300

    Create simple checkout sum

--- lib/marketplace/checkout.rb
+++ lib/marketplace/checkout.rb
@@ -1,4 +1,5 @@
 module Marketplace
+  # Controls our marketplace items
   class Checkout
     attr_reader :items
 
@@ -8,6 +9,10 @@ module Marketplace
 
     def scan(item)
       @items << item
+    end
+
+    def total
+      @items.inject(0) { |sum, item| sum + item.price }
     end
   end
 end
--- test/marketplace/checkout_test.rb
+++ test/marketplace/checkout_test.rb
@@ -3,18 +3,32 @@ require 'test_helper'
 module Marketplace
   class CheckoutTest < TestHelper
     def setup
-      @item = Item.new(code: '001', name: 'Lavender heart', price: BigDecimal.new('9.25'))
-
-      @checkout = Checkout.new
-      @checkout.scan(@item)
+      @items = [
+        Item.new(code: '001', name: 'Lavender heart', price: BigDecimal.new('9.25')),
+        Item.new(code: '002', name: 'Personalised cufflinks', price: BigDecimal.new('45.0')),
+        Item.new(code: '003', name: 'Kids T-shirt', price: BigDecimal.new('19.95'))
+      ]
     end
 
-    def test_should_have_items
-      assert @checkout.items.size > 0
+    def test_should_be_an_array_of_items
+      co = Checkout.new
+      co.scan(@items[0])
+      co.scan(@items[1])
+
+      assert co.items.size == 2
     end
 
-    def test_should_have_item_one
-      assert_equal @item, @checkout.items.select { |item| @item.code == item.code }[0]
+    def test_should_have_item_0
+      co = Checkout.new
+      co.scan(@items[0])
+      assert_equal @items[0], co.items.select { |item| @items[0].code == item.code }[0]
+    end
+
+    def test_should_have_simple_sum
+      co = Checkout.new
+      co.scan(@items[0])
+      co.scan(@items[1])
+      assert_equal BigDecimal.new('54.25'), co.total
     end
   end
 end
